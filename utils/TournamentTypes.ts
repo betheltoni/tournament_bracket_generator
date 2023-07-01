d6a9bea8b1fe8bd3a0fd32574a2ec1a2a1c5e310
bethel enunwa <enunwabethel@gmail.com>
2023-07-01 13:25:29 +0100

    fix: use prettier

--- utils/TournamentTypes.ts
+++ utils/TournamentTypes.ts
@@ -1,19 +1,19 @@
 interface Participant {
-  name: string;
+    name: string
 }
 
 interface Match {
-  participant1: string;
-  participant2: string;
-  winner: string | null;
+    participant1: string
+    participant2: string
+    winner: string | null
 }
 
 interface Bracket {
-  id?: string
-  matchdata: {
-    id: string,
-    rounds: Match[]
-  };
+    id?: string
+    matchdata: {
+        id: string
+        rounds: Match[]
+    }
 }
 
-export type { Participant, Match, Bracket };
+export type { Participant, Match, Bracket }
